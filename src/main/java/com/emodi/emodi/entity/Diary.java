e4d69d15dae3055c07aefad4bb34fcfd3a36b159
minisyu <iswater.kim@gmail.com>
2024-06-17 23:51:04 +0900

    refactor: 일기 날짜 필드 추가

--- src/main/java/com/emodi/emodi/entity/Diary.java
+++ src/main/java/com/emodi/emodi/entity/Diary.java
@@ -1,5 +1,7 @@
 package com.emodi.emodi.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -7,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +24,8 @@ public class Diary {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	private LocalDate date;
 
 	private String title;
 
