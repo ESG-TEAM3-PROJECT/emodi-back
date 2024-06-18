117ebad903be9bd14caeb7c563c4d8a0f516cf6f
minisyu <iswater.kim@gmail.com>
2024-06-18 23:37:40 +0900

    feat: 로그인 기능 구현
    - 비밀번호 검증 로직 추가
    - JWT 토큰 생성 기능 구현
    - 로그인 성공 시 Cookie 생성 및 response에 포함

--- src/main/java/com/emodi/emodi/controller/AuthController.java
+++ src/main/java/com/emodi/emodi/controller/AuthController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emodi.emodi.entity.User;
+import com.emodi.emodi.jwt.JwtProvider;
 import com.emodi.emodi.service.AuthService;
 import com.emodi.emodi.service.dto.SignupInfoRequest;
+import com.emodi.emodi.service.dto.request.LoginInfoRequest;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -17,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 	private final AuthService authService;
+	private final JwtProvider jwtProvider;
 
 	@PostMapping("/signup")
 	public ResponseEntity<String> signup(@RequestBody SignupInfoRequest request) {
@@ -24,5 +30,17 @@ public class AuthController {
 		return ResponseEntity
 			.status(HttpStatus.CREATED)
 			.body("회원가입이 완료되었습니다.");
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody LoginInfoRequest request, HttpServletResponse response) {
+		User user = authService.login(request);
+
+		String jwt = jwtProvider.generateToken(user.getId());
+		Cookie cookie = new Cookie("jwt", jwt);
+		response.addCookie(cookie);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body("로그인이 완료되었습니다.");
 	}
 }
--- src/main/java/com/emodi/emodi/repository/UserRepository.java
+++ src/main/java/com/emodi/emodi/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.emodi.emodi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.emodi.emodi.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByUsername(String username);
+
+	Optional<User> findByUsername(String username);
 }
