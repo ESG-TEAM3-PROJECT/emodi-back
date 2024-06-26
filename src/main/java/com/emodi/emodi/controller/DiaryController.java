34eef59a4948d88bd09e61940a36f6bbfdea823c
minisyu <iswater.kim@gmail.com>
2024-06-26 21:02:19 +0900

    refactor: 일기 작성 후 관련 데이터 응답 추가

--- src/main/java/com/emodi/emodi/controller/DiaryController.java
+++ src/main/java/com/emodi/emodi/controller/DiaryController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.emodi.emodi.entity.Diary;
 import com.emodi.emodi.jwt.JwtProvider;
 import com.emodi.emodi.service.DiaryService;
 import com.emodi.emodi.service.dto.request.WriteDiaryRequest;
+import com.emodi.emodi.service.dto.response.WriteDiaryResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,14 +24,20 @@ public class DiaryController {
 	private final JwtProvider jwtProvider;
 
 	@PostMapping("/diaries")
-	public ResponseEntity<String> writeDiary(
+	public ResponseEntity<WriteDiaryResponse> writeDiary(
 		@CookieValue("jwt") String token,
 		@RequestBody WriteDiaryRequest request
 	) {
 		Long userId = jwtProvider.verifyToken(token);
 
-		diaryService.writeDiary(userId, request);
+		Diary diary = diaryService.writeDiary(userId, request);
+
+		WriteDiaryResponse writeDiaryResponse = WriteDiaryResponse.toWriteDiaryResponse(
+			"일기 작성이 완료되었습니다.",
+			diary
+		);
+
 		return ResponseEntity.status(HttpStatus.CREATED)
-			.body("일기 작성이 완료되었습니다.");
+			.body(writeDiaryResponse);
 	}
 }
